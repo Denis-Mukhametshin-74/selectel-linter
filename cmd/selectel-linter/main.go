6513acb51a89c70410dbbdc4f6263e6d60d7b075
Denis-Mukhametshin-74 <Muhametshin-Den.05@yandex.ru>
2026-03-14 18:09:47 +0500

    feat: Add entry point

--- cmd/selectel-linter/main.go
+++ cmd/selectel-linter/main.go
@@ -1,3 +1,10 @@
 package main
 
-func main() {}
+import (
+	"github.com/Denis-Mukhametshin-74/selectel-linter/internal/analyzer"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+func main() {
+	singlechecker.Main(analyzer.Analyzer)
+}
